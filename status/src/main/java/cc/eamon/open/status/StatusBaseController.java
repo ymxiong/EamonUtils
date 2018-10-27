d142ec11c8b626d405772d60087d1c7bcd89c66f
ymxiong@whu.edu.cn <ymxiong@whu.edu.cn>
2018-10-27 17:33:47 +0800

    [feat]web return status control

---
+++ status/src/main/java/cc/eamon/open/status/StatusBaseController.java
@@ -0,0 +1,29 @@
+package cc.eamon.open.status;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Log4j2
+@ControllerAdvice
+public abstract class StatusBaseController {
+
+    /**
+     * Status异常处理
+     */
+    @ExceptionHandler(value = {Exception.class})
+    @ResponseBody
+    public Status statusExceptionHandler(HttpServletRequest request, HttpServletResponse response, Exception e){
+        if (e instanceof StatusException){
+            return StatusException.procExcp(e);
+        }
+        log.error(e.getMessage());
+        return new Status(false, 500, "Server Error", "Reading Log For Details");
+    }
+
+
+}
